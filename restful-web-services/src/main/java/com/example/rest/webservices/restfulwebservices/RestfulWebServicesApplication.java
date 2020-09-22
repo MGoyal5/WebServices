758b2b46f58afdf77646db30036d106d682b5a98
Manglesh Goyal <mangleshgoyal8@gmail.com>
2020-09-22 21:23:58 +0530

    child microservices

--- restful-web-services/src/main/java/com/example/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ restful-web-services/src/main/java/com/example/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -6,8 +6,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.web.filter.CommonsRequestLoggingFilter;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 import utility.exceptionhandler.RestExceptionHandler;
+import utility.logger.CustomRequestBodyAdviceAdapter;
+import utility.logger.CustomResponseBodyAdviceAdapter;
+import utility.logger.LogInterceptor;
+import utility.logger.LoggingService;
+import utility.logger.LoggingServiceImpl;
 
 @SpringBootApplication
 @ComponentScan({"com.example.rest.webservices.restfulwebservices"})
@@ -22,5 +29,37 @@ public class RestfulWebServicesApplication {
 	public RestExceptionHandler restExceptionHandler()
 	{
 		return new RestExceptionHandler();
+	}
+	
+	@Bean
+	public LogInterceptor logInterceptor() {
+		
+		return new LogInterceptor();
+	}
+	
+	@Bean
+	public CustomRequestBodyAdviceAdapter customRequestAdviceAdapter() {
+		return new CustomRequestBodyAdviceAdapter();
+	}
+	
+	@Bean
+	public CustomResponseBodyAdviceAdapter customResponseBodyAdviceAdapter() {
+		return new CustomResponseBodyAdviceAdapter();
+	}
+	
+	@Bean
+	public LoggingService loggingService()
+	{
+		return new LoggingServiceImpl();
+	}
+	
+	@Bean
+	public CommonsRequestLoggingFilter requestLoggingFilter() {
+	    CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+	    loggingFilter.setIncludeClientInfo(true);
+	    loggingFilter.setIncludeQueryString(true);
+	    loggingFilter.setIncludePayload(true);
+	    loggingFilter.setIncludeHeaders(false);
+	    return loggingFilter;
 	}
 }
